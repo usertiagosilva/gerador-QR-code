ec0af68ac9ca8f1f17351214fa817e4eafd7df1e
usertiagosilva <tiago_ferreira.s@hotmail.com>
2024-03-24 14:38:36 -0300

    commit v1

--- gerador_QR_code/js/scripts.js
+++ gerador_QR_code/js/scripts.js
@@ -1,1 +1,45 @@
-console.log("teste")
+// Selecionando elementos
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input");
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+// Gerar QR Code
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    // Validação para quando não tiver valor
+    if (!qrCodeInputValue) return;
+
+    // Esperar resposta
+    qrCodeBtn.innerText = "Gerando código...";
+
+    // API para gerar imagem QR Code
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    // Mostrar imagem quando carregar
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código criado!";
+    });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
+
+// Validar tecla enter
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.code === "Enter"){
+        generateQrCode();
+    }
+});
+
+// Limpar área do QR Code
+qrCodeInput.addEventListener("keyup", () => {
+    if(!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR Code";
+    }
+})
